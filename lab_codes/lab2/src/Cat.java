58655a2f773168b5b1fe9b98f5d24a56dfc43f2a
sendrosand <sendrosaster@gmail.com>
2022-10-18 20:42:38 +0300

    lab 2 first examples

---
+++ lab_codes/lab2/src/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+    String name, colour;
+    int age;
+
+    public static void main(String[] arguments){
+        talk();
+    }
+    static void talk() {
+        System.out.println("meow!");
+    }
+}
+
