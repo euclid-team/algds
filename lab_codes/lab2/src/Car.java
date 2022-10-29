fb04e64e0f31da810930faccd7796803f4916d9b
sendrosand <sendrosaster@gmail.com>
2022-10-29 16:32:58 +0300

    lab2 code changes

--- lab_codes/lab2/src/Car.java
+++ lab_codes/lab2/src/Car.java
@@ -1,12 +1,20 @@
-public class Car {
-    String model;
-    double currentSpeed;
+public class Car extends Vehicle {
+    private String model;
+//    private double currentSpeed;
 
-    public Car(String carModel, double speed) {
+    public Car(String carModel, double speed, int wheels) {
         model = carModel;
-          currentSpeed=speed;
+        super.Vehicle(wheels, speed);
     }
-//
+
+    public Car(String carModel) {
+        model = carModel;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
 //    public void setSpeed(double newSpeed) {
 //        currentSpeed = newSpeed;
 //    }
@@ -20,12 +28,15 @@ public class Car {
 class MainClass{
     public static void main(String[] args) {
         Car car1;
-        car1 = new Car("Toyota Corolla", 54);
-        System.out.println(car1.model);
+        car1 = new Car("Toyota Corolla", 185, 4);
+        System.out.println(car1.speed);
+//        car1.setSpeed(196);
+//        System.out.println(car1.getSpeed());
 
-        Car[] carinst = new Car[3];
-        carinst[0]=new Car("Volvo", 189);
-        System.out.println(carinst[0].currentSpeed);
+
+//        Car[] carinst = new Car[3];
+//        carinst[0]=new Car("Volvo", 189);
+//        System.out.println(carinst[0].getSpeed());
 
     }
 }
