7d116181f72754ce29aea395b705ee3dbe3000d8
sendrosand <sendrosaster@gmail.com>
2022-11-18 18:06:58 +0200

    quick sort example

---
+++ lab_codes/lab5/src/QuickSort.java
@@ -0,0 +1,50 @@
+public class QuickSort {
+    int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low-1);
+        for (int j=low; j<high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+        return i+1;
+    }
+
+    void sort(int arr[], int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = partition(arr, low, high);
+            sort(arr, low, pi-1);
+            sort(arr, pi+1, high);
+        }
+    }
+
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        MergeSort ob = new MergeSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90}, len = arr.length-1;
+        ob.sort(arr, 0, len );
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+
+}
