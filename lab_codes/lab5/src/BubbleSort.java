da82127b8bb72253acd6c5d84567894dd0d758c4
sendrosand <sendrosaster@gmail.com>
2022-11-21 15:47:56 +0200

    bubble sort example

--- lab_codes/lab5/src/BubbleSort.java
+++ lab_codes/lab5/src/BubbleSort.java
@@ -2,7 +2,9 @@ public class BubbleSort {
     void bubbleSort(int arr[])
     {
         int n = arr.length;
+        boolean flag;
         for (int i = 0; i < n-1; i++)
+            flag=true;
             for (int j = 0; j < n-i-1; j++)
                 if (arr[j] > arr[j+1])
                 {
@@ -10,7 +12,9 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    flag=false;
                 }
+            if (flag) return;
     }
 
     /* Prints the array */
