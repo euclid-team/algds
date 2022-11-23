124743f29a9c78ba36f655a22b44e3ec24faf73a
sendrosand <sendrosaster@gmail.com>
2022-11-24 00:33:43 +0200

    maxheapify example

---
+++ lab_codes/lab6/src/MaxHeapify.java
@@ -0,0 +1,45 @@
+public class MaxHeapify {
+    void heapify(int arr[], int N, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, N, largest);
+        }
+    }
+
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        MaxHeapify ob = new MaxHeapify();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90}, N = arr.length;
+        for (int i = N / 2 - 1; i >= 0; i--)
+            ob.heapify(arr, N, i);
+        System.out.println("Max Heap");
+        ob.printArray(arr);
+    }
+}
